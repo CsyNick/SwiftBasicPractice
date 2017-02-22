cbd16fb948fceac271372400382ced5d769d8809
Nick Chung <chsy0208@gmail.com>
2017-02-22 11:45:32 +0800

    intial

---
+++ UIKit/uikit:uikit_intro:/Uikit_intro/Uikit_intro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Uikit_intro
+//
+//  Created by Nick Chung on 2017/2/22.
+//  Copyright © 2017年 Nick Chung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
